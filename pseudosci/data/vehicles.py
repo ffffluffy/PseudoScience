21996092bd08e5bb2db1ca54ea139ec9b63b5f6f
Lucidiot <erwanrct@gmail.com>
2017-02-12 00:39:46 +0100

    Ajout des informations du métro

---
+++ pseudosci/data/vehicles.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""Véhicules aux propriétés connues et déjà utilisés dans des études
+précédentes. Bien vérifier l'exactitude des informations en fonction du
+contexte du calcul à effectuer."""
+
+from ..transport.vehicle import Vehicle
+from ..units import Velocity, Acceleration
+
+# Métro lillois (VAL 206) - https://fr.wikipedia.org/wiki/VAL_206
+lille_metro = Vehicle(velocity=Velocity(kph=80),
+                      accel=Acceleration(mpss=1.4),
+                      brake=Acceleration(mpss=-1.3))
