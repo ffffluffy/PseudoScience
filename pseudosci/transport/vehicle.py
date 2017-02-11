293e41eafc647aad2e8917eaa0fb4f0753eda8bb
Lucidiot <erwanrct@gmail.com>
2017-02-11 19:17:02 +0100

    Tests unitaires de Vehicle, close #7

--- pseudosci/transport/vehicle.py
+++ pseudosci/transport/vehicle.py
@@ -9,10 +9,13 @@ class Vehicle:
     """Décrit un moyen de locomotion standard."""
 
     def __init__(self, velocity=None, accel=None, brake=None):
+        self.velocity = velocity
+        self.accel = accel
+        self.brake = brake
         if not velocity or type(velocity) is not Velocity:
             raise TypeError("Le paramètre obligatoire ``velocity`` doit être "
                             "une instance de pseudosci.units.Velocity.")
-        if velocity.m <= 0:
+        if velocity.mps <= 0:
             raise ValueError("La vélocité du véhicule doit être strictement "
                              "positive.")
         if not accel:
@@ -23,17 +26,16 @@ class Vehicle:
         elif accel.mpss < 0:
             raise ValueError("L'accélération du véhicule doit être positive "
                              "ou nulle.")
-        if not brake:
-            self.brake = self.accel
+        if accel and not brake:
+            self.brake = accel
+        elif not accel and not brake:
+            self.brake = Acceleration(mpss=0)
         elif type(brake) is not Acceleration:
             raise TypeError("Le paramètre optionnel ``brake`` doit être une "
                             "instance de pseudosci.units.Acceleration.")
         elif brake.mpss <= 0:
             raise ValueError("La décélération du véhicule doit être "
                              "strictement positive.")
-        self.velocity = velocity
-        self.accel = accel
-        self.brake = brake
 
     def move(self, distance):
         if self.accel.mpss <= 0:
