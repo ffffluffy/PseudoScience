5d2d8e8c6dc163ac4261eba88e67890c2d971a78
Lucidiot <erwanrct@gmail.com>
2017-02-12 23:11:21 +0100

    Classe Unit et refactoring des unités, close #17

--- pseudosci/units.py
+++ pseudosci/units.py
@@ -15,7 +15,81 @@ KPH_MPS = 1 / 3.6
 KPHS_MPSS = 1 / 3.6
 
 
-class Distance(object):
+class Unit(object):
+    """Décrit une unité de mesure."""
+
+    def __init__(self, value):
+        self.value = float(value)
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return '<{0} {1}>'.format(type(self).__name__, self)
+
+    def __int__(self):
+        return int(self.value)
+
+    def __float__(self):
+        return float(self.value)
+
+    def __abs__(self):
+        return type(self)(abs(self.value))
+
+    def __pos__(self):
+        return type(self)(+self.value)
+
+    def __neg__(self):
+        return type(self)(-self.value)
+
+    def __add__(self, other):
+        if type(other) is int or type(other) is float:
+            return type(self)(self.value + other)
+        elif type(self) is type(other):
+            return type(self)(self.value + other.value)
+        else:
+            raise TypeError("Il n'est pas possible d'additionner deux unités "
+                            "différentes.")
+    __radd__ = __add__
+
+    def __sub__(self, other):
+        if type(other) is int or type(other) is float:
+            return type(self)(self.value - other)
+        elif type(self) is type(other):
+            return type(self)(self.value - other.value)
+        else:
+            raise TypeError("Il n'est pas possible de soustraire deux unités "
+                            "différentes.")
+    __rsub__ = __sub__
+
+    def __mul__(self, other):
+        if type(other) is int or type(other) is float:
+            return type(self)(self.value * other)
+        else:
+            raise TypeError("Il n'est pas possible de multiplier deux unités "
+                            "différentes.")
+    __rmul__ = __mul__
+
+    def __div__(self, other):
+        if type(other) is int or type(other) is float:
+            return type(self)(self.value / other)
+        elif type(self) is type(other):
+            return (self.value / other.value)
+        else:
+            raise TypeError("Il n'est pas possible de diviser deux unités "
+                            "différentes.")
+
+    def __floordiv__(self, other):
+        if type(other) is int or type(other) is float:
+            return type(self)(self.value // other)
+        elif type(self) is type(other):
+            return (self.value // other.value)
+        else:
+            raise TypeError("Il n'est pas possible de diviser deux unités "
+                            "différentes.")
+
+
+class Distance(Unit):
     """Décrit une mesure de distance. L'unité correspondante du système
     international est le mètre (m).\n
     Utilisez l'un des paramètres suivants pour initialiser la classe :\n
@@ -26,19 +100,22 @@ class Distance(object):
 
     def __init__(self, m=None, km=None, au=None, ly=None):
         if m is not None:
-            self.m = float(m)
+            Unit.__init__(self, float(m))
         elif km is not None:
-            self.m = float(km) * KM_M
+            Unit.__init__(self, float(km) * KM_M)
         elif au is not None:
-            self.m = float(au) * AU_M
+            Unit.__init__(self, float(au) * AU_M)
         elif ly is not None:
-            self.m = float(ly) * LY_M
+            Unit.__init__(self, float(ly) * LY_M)
         else:
             raise ValueError(
                 "Pour construire une unité de distance, fournissez m, km, au "
                 "ou ly.")
 
     def __getattr__(self, name):
+        if name.lower() == 'm':
+            self.m = m = self.value
+            return m
         if name.lower() == 'km':
             self.km = km = self.m / KM_M
             return km
@@ -51,77 +128,24 @@ class Distance(object):
         else:
             raise AttributeError("No attribute named %r" % (name.lower(),))
 
-    def __str__(self):
-        return str(self.m)
-
-    def __repr__(self):
-        return '<{0} {1}>'.format(type(self).__name__, self)
-
-    def __int__(self):
-        return int(self.m)
-
-    def __float__(self):
-        return float(self.m)
-
-    def __abs__(self):
-        return Distance(m=abs(self.m))
-
-    def __neg__(self):
-        return Distance(m=-self.m)
-
-    def __add__(self, other):
-        if type(other) is Distance:
-            return Distance(m=self.m + other.m)
-        else:
-            raise TypeError(
-                "Une distance ne peut être ajoutée qu'à une distance.")
-
-    def __sub__(self, other):
-        if type(other) is Distance:
-            return Distance(m=self.m - other.m)
-        else:
-            raise TypeError(
-                "Une distance ne peut être soustraite qu'à une distance.")
-
-    def __mul__(self, other):
-        if type(other) is int or type(other) is float:
-            return Distance(m=self.m * other)
-        else:
-            raise NotImplementedError(
-                "La multiplication de distances n'est pas implémentée.")
-
-    __rmul__ = __mul__
-
     def __div__(self, other):
-        if type(other) is int or type(other) is float:
-            return Distance(m=self.m / other)
-        elif type(other) is Time:
+        if type(other) is Time:
             return Velocity(mps=self.m / other.s)
-        elif type(other) is Distance:
-            return self.m / other.m
         elif type(other) is Velocity:
             return Time(s=self.m / other.mps)
         else:
-            raise TypeError(
-                "Une distance ne peut être divisée que par une distance, une "
-                "durée, une vitesse, ou un nombre.")
+            return Unit.__div__(self, other)
 
     def __floordiv__(self, other):
-        if type(other) is int or type(other) is float:
-            return Distance(m=self.m // other)
-        elif type(other) is Time:
+        if type(other) is Time:
             return Velocity(mps=self.m // other.s)
-        elif type(other) is Distance:
-            return self.m // other.m
         elif type(other) is Velocity:
             return Time(s=self.m // other.mps)
         else:
-            raise TypeError(
-                "Une distance ne peut être divisée que par une distance, une "
-                "durée, une vitesse ou un nombre.")
+            return Unit.__floordiv__(self, other)
 
 
-class Time(object):
+class Time(Unit):
     """Décrit une mesure temporelle. L'unité correspondante du système
     international est la seconde (s).\n
     Utilisez l'un des paramètres suivants pour initialiser la classe :\n
@@ -132,21 +156,23 @@ class Time(object):
 
     def __init__(self, s=None, m=None, h=None, d=None):
         if s is not None:
-            self.s = float(s)
+            Unit.__init__(self, float(s))
         elif m is not None:
-            self.s = float(m) * MIN_S
+            Unit.__init__(self, float(m) * MIN_S)
         elif h is not None:
-            self.s = float(h) * H_S
+            Unit.__init__(self, float(h) * H_S)
         elif d is not None:
-            self.s = float(d) * D_S
+            Unit.__init__(self, float(d) * D_S)
         else:
             raise ValueError(
                 "Pour construire une unité de temps, fournissez s, m, h ou d.")
 
     def __getattr__(self, name):
-        if name.lower() == 'm' or name.lower() == 'min':
-            self.m = m = self.s / MIN_S
-            self.min = self.m
+        if name.lower() == 's':
+            self.s = s = self.value
+            return s
+        elif name.lower() == 'm' or name.lower() == 'min':
+            self.min = self.m = m = self.s / MIN_S
             return m
         elif name.lower() == 'h':
             self.h = h = self.s / H_S
@@ -157,124 +183,40 @@ class Time(object):
         else:
             raise AttributeError("No attribute named %r" % (name.lower(),))
 
-    def __str__(self):
-        return str(self.s)
-
-    def __repr__(self):
-        return '<{0} {1}>'.format(type(self).__name__, self)
-
-    def __int__(self):
-        return int(self.s)
-
-    def __float__(self):
-        return float(self.s)
-
-    def __abs__(self):
-        return Time(s=abs(self.s))
-
-    def __neg__(self):
-        return Time(s=-self.s)
-
-    def __add__(self, other):
-        if type(other) is Time:
-            return Time(s=self.s + other.s)
-        else:
-            raise TypeError("Une durée ne peut être ajoutée qu'à une durée.")
-
-    def __sub__(self, other):
-        if type(other) is Time:
-            return Time(s=self.s - other.s)
-        else:
-            raise TypeError(
-                "Une durée ne peut être soustraite qu'à une durée.")
-
     def __mul__(self, other):
-        if type(other) is int or type(other) is float:
-            return Time(s=self.s * other)
-        elif type(other) is Velocity:
+        if type(other) is Velocity:
             return Distance(m=self.s * other.mps)
         elif type(other) is Acceleration:
             return Velocity(mps=self.s * other.mpss)
         else:
-            raise TypeError(
-                "Une durée ne peut être multipliée que par une vitesse, une "
-                "accélération ou un nombre.")
+            return Unit.__mul__(self, other)
 
     __rmul__ = __mul__
 
-    def __div__(self, other):
-        if type(other) is int or type(other) is float:
-            return Time(s=self.s / other)
-        elif type(other) is Time:
-            return self.s / other.s
-        else:
-            raise TypeError(
-                "Une durée ne peut être divisée que par une durée ou un "
-                "nombre.")
 
-    def __floordiv__(self, other):
-        if type(other) is int or type(other) is float:
-            return Time(s=self.s // other)
-        elif type(other) is Time:
-            return self.s // other.s
-        else:
-            raise TypeError(
-                "Une durée ne peut être divisée que par une durée ou un "
-                "nombre.")
-
-
-class Velocity(object):
+class Velocity(Unit):
     """Décrit une vitesse, ou vélocité. L'unité correspondante du système
     international est le mètre par seconde (m.s^-1).\n
     Utilisez soit ``mps=``, soit ``kph=`` pour l'initialiser."""
 
     def __init__(self, mps=None, kph=None):
         if mps is not None:
-            self.mps = float(mps)
+            Unit.__init__(self, float(mps))
         elif kph is not None:
-            self.mps = float(kph) * KPH_MPS
+            Unit.__init__(self, float(kph) * KPH_MPS)
         else:
             raise ValueError(
                 "Pour construire une unité de vitesse, fournissez mps ou kph.")
 
     def __getattr__(self, name):
-        if name.lower() == 'kph':
+        if name.lower() == 'mps':
+            self.mps = mps = self.value
+            return mps
+        elif name.lower() == 'kph':
             self.kph = kph = self.mps / KPH_MPS
             return kph
         else:
             raise AttributeError("No attribute named %r" % (name.lower(),))
-
-    def __str__(self):
-        return str(self.mps)
-
-    def __repr__(self):
-        return '<{0} {1}>'.format(type(self).__name__, self)
-
-    def __int__(self):
-        return int(self.mps)
-
-    def __float__(self):
-        return float(self.mps)
-
-    def __abs__(self):
-        return Velocity(mps=abs(self.mps))
-
-    def __neg__(self):
-        return Velocity(mps=-self.mps)
-
-    def __add__(self, other):
-        if type(other) is Velocity:
-            return Velocity(mps=self.mps + other.mps)
-        else:
-            raise TypeError(
-                "Une vitesse ne peut être ajoutée qu'à une vitesse.")
-
-    def __sub__(self, other):
-        if type(other) is Velocity:
-            return Velocity(mps=self.mps - other.mps)
-        else:
-            raise TypeError(
-                "Une vitesse ne peut être soustraite qu'à une vitesse.")
 
     def __mul__(self, other):
         if type(other) is int or type(other) is float:
@@ -282,42 +224,28 @@ class Velocity(object):
         elif type(other) is Time:
             return Distance(m=self.mps * other.s)
         else:
-            raise TypeError(
-                "Une vitesse ne peut être multipliée que par une durée ou "
-                "un nombre.")
+            return Unit.__mul__(self, other)
 
     __rmul__ = __mul__
 
     def __div__(self, other):
-        if type(other) is int or type(other) is float:
-            return Velocity(mps=self.mps / other)
-        elif type(other) is Velocity:
-            return self.mps / other.mps
-        elif type(other) is Time:
+        if type(other) is Time:
             return Acceleration(mpss=self.mps / other.s)
         elif type(other) is Acceleration:
             return Time(s=self.mps / other.mpss)
         else:
-            raise TypeError(
-                "Une vitesse ne peut être divisée que par une durée, une "
-                "vitesse, une accélération ou un nombre.")
+            return Unit.__div__(self, other)
 
     def __floordiv__(self, other):
-        if type(other) is int or type(other) is float:
-            return Velocity(mps=self.mps // other)
-        elif type(other) is Velocity:
-            return self.mps // other.mps
-        elif type(other) is Time:
+        if type(other) is Time:
             return Acceleration(mpss=self.mps // other.s)
         elif type(other) is Acceleration:
             return Time(s=self.mps // other.mpss)
         else:
-            raise TypeError(
-                "Une vitesse ne peut être divisée que par une durée, une "
-                "vitesse, une accélération ou un nombre.")
+            return Unit.__floordiv__(self, other)
 
 
-class Acceleration(object):
+class Acceleration(Unit):
     """Décrit une accélération. L'unité correspondante du système international
     est le mètre par seconde carrée (m.s^-2).\n
     Utilisez soit un paramètre ``mpss=``, soit un paramètre ``kphs=`` pour
@@ -325,82 +253,28 @@ class Acceleration(object):
 
     def __init__(self, mpss=None, kphs=None):
         if mpss is not None:
-            self.mpss = float(mpss)
+            Unit.__init__(self, float(mpss))
         elif kphs is not None:
-            self.mpss = float(kphs) * KPHS_MPSS
+            Unit.__init__(self, float(kphs) * KPHS_MPSS)
         else:
             raise ValueError(
                 "Pour construire une unité d'accélération, fournissez mpss ou "
                 "kphs.")
 
     def __getattr__(self, name):
-        if name.lower() == 'kphs':
+        if name.lower() == 'mpss':
+            self.mpss = mpss = self.value
+            return mpss
+        elif name.lower() == 'kphs':
             self.kphs = kphs = self.mpss / KPHS_MPSS
             return kphs
         else:
             raise AttributeError("No attribute named %r" % (name.lower(),))
 
-    def __str__(self):
-        return str(self.mpss)
-
-    def __repr__(self):
-        return '<{0} {1}>'.format(type(self).__name__, self)
-
-    def __int__(self):
-        return int(self.mpss)
-
-    def __float__(self):
-        return float(self.mpss)
-
-    def __abs__(self):
-        return Acceleration(mpss=abs(self.mpss))
-
-    def __neg__(self):
-        return Acceleration(mpss=-self.mpss)
-
-    def __add__(self, other):
-        if type(other) is Acceleration:
-            return Acceleration(mpss=self.mpss + other.mpss)
-        else:
-            raise TypeError(
-                "Une accélération ne peut être ajoutée qu'à une accélération.")
-
-    def __sub__(self, other):
-        if type(other) is Acceleration:
-            return Acceleration(mpss=self.mpss - other.mpss)
-        else:
-            raise TypeError(
-                "Une accélération ne peut être soustraite qu'à une "
-                "accélération.")
-
     def __mul__(self, other):
-        if type(other) is int or type(other) is float:
-            return Acceleration(mpss=self.mpss * other)
-        elif type(other) is Time:
+        if type(other) is Time:
             return Velocity(mps=self.mpss * other.s)
         else:
-            raise TypeError(
-                "Une accélération ne peut être multipliée que par une durée "
-                "ou un nombre.")
+            return Unit.__mul__(self, other)
 
     __rmul__ = __mul__
-
-    def __div__(self, other):
-        if type(other) is int or type(other) is float:
-            return Acceleration(mpss=self.mpss / other)
-        elif type(other) is Acceleration:
-            return self.mpss / other.mpss
-        else:
-            raise TypeError(
-                "Une accélération ne peut être divisée que par une "
-                "accélération ou un nombre.")
-
-    def __floordiv__(self, other):
-        if type(other) is int or type(other) is float:
-            return Acceleration(mpss=self.mpss // other)
-        elif type(other) is Acceleration:
-            return self.mpss // other.mpss
-        else:
-            raise TypeError(
-                "Une accélération ne peut être divisée que par une "
-                "accélération ou un nombre.")
